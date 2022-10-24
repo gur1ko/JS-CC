f07b3d3906f6a142fc740574c7d44a839c3041ee
GURAMI KIKNADZE <gurikiknadze@gmail.com>
2022-10-24 22:03:46 +0400

    started JS journey, covered data types and variables, covering conditionals, and i'm motivated af, be aware of me 3:)

--- app.js
+++ app.js
@@ -1,1 +1,136 @@
-console.log('hello, ' + 'Gurami')
+////////// DATA TYPES & VARIABLES/////////////
+
+// string example
+
+/* console.log("Hello World" [10])
+console.log('12345'.length) */
+
+
+/* let str = 'Gurami'
+console.log(str[str.length - 1]) */
+
+// VARIABLES
+
+/** 
+var
+let
+const
+*/ 
+
+/* const isRaining = true
+const temperature = 30
+let planet = 'Earth'
+
+temperature = temperature + 2
+console.log(temperature) */
+
+
+/* let celsius = 10;
+let farenheit = celsius * 1.8 + 32;
+
+console.log(farenheit) */
+
+
+/* let bool = '1' !== 1 
+
+console.log(bool)
+ */
+
+
+
+////////////CONDITIONALS/////////////
+
+/* let subscribed = true
+let loggedIn = false
+
+if (subscribed === true) {
+    console.log('show the video')
+}
+else if (loggedIn === true) {
+    console.log('tell user to upgrade their subscription')
+}
+else {
+    console.log('tell user to log into account')
+}
+ */
+
+
+
+
+/* let cash = 45
+let price = 40
+let difference = cash - price
+
+if (cash > price) {
+    console.log(`you paid extra - here is ${difference} dollars change`)
+
+}
+
+else if (cash === price) {
+    console.log('you paid theexact amount, have a nice day')
+}
+
+else {
+    console.log(`not enough money - you still owe ${difference * -1}  dollars`)
+}
+ */
+// && - and
+// || - or
+
+/* let cash = 50
+let price = 40
+let isStoreOpen = false
+ */
+/* if (cash >= price || !isStoreOpen) {
+    console.log('print the receipt')
+}
+ */
+
+
+// '!isStoreOpen' is same as 'isStoreOpen === false' and 'isStoreOpen' is same as 'isStoreOpen === true'
+
+/* 
+let val = "gurami";
+
+if (val) {
+    console.log("truthy value", !!val)
+}
+else {
+    console.log("falsy value", !!val)
+} */
+
+////Ternary Operators - 52:58///
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
