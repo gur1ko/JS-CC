912927b023faeff33cfb709b61d69788ed56429e
GURAMI KIKNADZE <gurikiknadze@gmail.com>
2022-10-25 23:03:16 +0400

    exercising and becoming above average person

--- app.js
+++ app.js
@@ -102,14 +102,104 @@ else {
 ////Ternary Operators - 52:58///
 
 
+/* let hot = true
+
+hot ? console.log('weather is hot outside') : console.log('weather is cold')
+ */
 
 
+/* let subscribed = true
+let loggedIn = false
+let str = subscribed || loggedIn ? 'show the video' : 'hide the video'
+console.log(str);
+ */
+// 4 short codes
 
 
+/* let cash = 50
+let price = 40
+let isStoreOpen = true
+
+let str = isStoreOpen && (cash >= price) ? 'give receipt' : 'do not give receipt'
+console.log(str)
+ */
 
 
+/////////////LOOPS////////////
+
+// DRY = Don't Repeat Yourself
+
+/**
+ * FOR LOOP >>>>
+ * while loop
+ * do whule loop
+*/
+
+/* let count = 1
+
+while (count <= 100) {
+    console.log(count)
+    count = count + 1;
+}
+
+ */
+
+//   set i to 0 ; less than; plus 1 or += n
+/* for (let i = 0; i < 100; i++) {
+console.log(i + 1);
+}
+
+ */
+//////////EXERCISE 1 ////////
+
+/**
+ * Write a for-loop that loops through 1 to 20
+ * if the number is divisible by 3, print "Frontend"
+ * if the number is divisible by 5, print "Simplified"
+ * if the number is divisible by 3 and 5, print "Frontend Simplified"
+ * if the number is *not* divisible 3 or 5, print the number  */  
+
+//MY SOLUTION
+/* for (let i = 1;i <= 20; i++) {
+    console.log(i + 1);
+    let str = i % 3 && i && 5 ? 'Frontend Simplified' : i
+    console.log(str)
+    let stri = i % 3 == 0 ? 'Frontend' : i
+    console.log(stri)
+    let strin = i % 5 == 0 ? 'Simplified' : i
+    console.log(strin)
+
+} */
+// CORRECT SOLUTION
+
+/* for (let i = 1; i <= 20; ++i) {
+    
+    if (i % 5 === 0 && i % 3 === 0) {
+        console.log(`${i} -> Frontend Simplified`)
+    }
+    else if (i % 3 === 0) {
+        console.log(`${i} -> Frontend`)
+    }
+
+    else if (i % 5 === 0) {
+        console.log(`${i} -> Simplified`)
+    }
+    
+    else {
+        console.log(i)
+    }
+} */
 
 
+////////EXERCISE 2///////
+/**
+ * Print out every character from the string:
+ * "Frontend Simplified" */  
+
+let str = "Frontend Simplified"
+for (let i = 1; i <= console.log(str.length); ++i) {
+console.log(i)
+}
 
 
 
