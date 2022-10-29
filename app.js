fabdf636dfa229eaf40a52b0b0a7f9502a6ec9b9
GURAMI KIKNADZE <gurikiknadze@gmail.com>
2022-10-29 21:34:50 +0400

    finally finishedcrash course

--- app.js
+++ app.js
@@ -281,7 +281,7 @@ console.log(celsiusToFarenheit(0)) */
 ///////////ARRAYS//////////
 ///////////////////////////
 
-let arr = [20, 30, 40, 50, 100]
+/* let arr = [20, 30, 40, 50, 100] */
 
 // First element of array
 
@@ -295,22 +295,270 @@ console.log(arr[arr.length - 1])
 
 arr.push(200) */
 // [20, 30, 40, 50, 100, true]
-let newArr = arr.filter((element) => {
-    console.log(element)
-    if (element < 50){
-    return true;
-}
-})
+//IIF we have only one element in our statement we can use "element" without additional bracket.
+/* let newArr = arr.filter(element => element <= 50
+)
+
 
 console.log(newArr)
 
+ */
+
+////exercise///
+/**
+ * filter out all the 'FAIL' elements in an array
+ * 
+ *  */
+
+
+/* let grades = ['A+', 'A', 'FAIL']
+ */
+/* let goodGrades = grades.filter(element => element !== 'FAIL')
+
+console.log(goodGrades)
+
+ */
+// create a new empty array 'goodGrades'
+/* let goodGrades = []
+for (let i = 0; i < grades.length; ++i) {
+    if (grades[i] !== 'FAIL') {
+        goodGrades.push(grades[i]);
+    } */
+    //add the current element onto 'goodGrades' only if 
+    //the current element is not equal to 'FAIL'
+
+/*     console.log(goodGrades);
+}  */
+
+/// returns each element in what we want 
+/* let arr = [1, 4, 9, 16]
+
+let newArray = arr.map(element => 'dog')
+
+console.log(newArray)
+
+ */
+
+/////exercise///
+////turn each element into cent////
+
+/* let dollars = [1,5,10,3]
+
+let cents = dollars.map(element => element *100 )
+console.log(cents) */
+
+
+////now without .map method
+/*
+for (let i = 0; i < grades.length; ++i) {
+    if (grades[i] !== 'FAIL') {
+        goodGrades.push(grades[i]);
+    } */
+    //add the current element onto 'goodGrades' only if 
+    //the current element is not equal to 'FAIL'
+
+/*     console.log(goodGrades);
+}  */
+
+/* let dollars = [1, 5, 10, 3];
+
+let cents = [];
+
+for (let i = 0; i < dollars.length; ++i) {
+    cents.push(dollars[i] * 100);
+
+}
+console.log(cents) */
+
+
+
+
+///////OBJECTS////////////
+///////////////////////////
+////////////////////////
+////////////////////////////
+
+// used to store multiple properties in one variable
+
+/* let userFirstName = 'Gurami'
+let userLastName = 'Kiknadze'
+let UserDiscordId = 'Gur1ko'
+let userSubscriptionStatus = 'VIP'
+ */
+
+
+
+/* let users = [
+    {
+    username: 'Gurami',
+    email: 'blabla@gmail.com',
+    password: 'test123',
+    userSubscriptionStatus: 'VIP',
+    userDiscordId: 'Gur1ko',
+    lessonsCompleted: [0, 1]
+}, 
+{
+    username: 'george',
+    email: 'blabla@gmail.com',
+    password: 'test321',
+    userSubscriptionStatus: 'VIP',
+    userDiscordId: 'georgur1ko',
+    lessonsCompleted: [0, 1]
+},
+];
+function login(email, password) {   
+    for (let i = 0; i < users.length; ++i) {
+    console.log('this ran')
+
+    if (users[i].email === email) {
+    console.log(users[i]);
+    if (users[i].password === password) {
+    console.log(email, password) 
+    console.log('log the user in - the derails are correct')
+            }
+    else {
+    console.log('password incorrect, try again')
+            }
+        }
+    return;
+    }
+    console.log('couldnt find an email that matches')
+    }
+
+
+login('blabla@gmail.com', 'test123');
+
+ */
+
+let users = [
+{
+    username: 'Gurami',
+    email: 'gg@gmail.com',
+    password: 'test123',
+    userSubscriptionStatus: 'VIP',
+    userDiscordId: '123',
+    lessonsCompleted: [0, 2, 5, 10]
+}, 
+{
+    username: 'rami',
+    email: 'gg@gmail.com',
+    password: 'test123',
+    userSubscriptionStatus: 'VIP',
+    userDiscordId: '123',
+    lessonsCompleted: [0, 2, 5, 10]
+},
+];
+/* function login(username, email, password, userSubscriptionStatus, usersDiscordId, lessonsCompleted) {
+    for (let i = 0; i < users.length; ++1){
+        if (user[i].username === username) {
+            console.log(users[i]);
+        if (user[i].email === email) {
+            console.log(users[i]);
+        if (user[i].password === password) {
+            console.log(users[i]);
+        if (user[i].userSubscriptionStatus === userSubscriptionStatus) {
+            console.log(users[i]);
+        if (user[i].userDiscordId === userDiscordId) {
+            console.log(users[i]);
+        if (user[i].lessonsCompleted === lessonsCompleted) {
+            console.log(users[i]);
+            console.log(username, email, password, userSubscriptionStatus, userDiscordId, lessonsCompleted)
+            console.log('log the user in - the details are correct')
+            
+        }
+        else {
+            console.log('incorrect details')
+        }
+    }
+}
+        }
+    }
+    return;
+}
+
+}
+login('Gurami', 'gg@gmail.com', 'test123', 'VIP', '123', [0, 2, 5, 10]) */ 
+
+/* if (users[i].email === email) {
+    console.log(users[i]);
+    if (users[i].password === password) {
+    console.log(email, password) 
+    console.log('log the user in - the derails are correct')
+            }
+    else {
+    console.log('password incorrect, try again')
+            }
+        }
+    return;
+    }
+    console.log('couldnt find an email that matches')
+    }
+
+
+login('blabla@gmail.com', 'test123');
+ */
+
+
+
+/* function register(
+    namme,
+    email,
+    password,
+    userSubscriptionStatus,
+    discordId,
+    lessonsCompleted
+) {
+    users.push(user);
+}
+
+register( {
+    username: "Guri",
+    email: "guri@gmail.com",
+    password:"12345678",
+    userSubscriptionStatus:"VIP",
+    userDiscordId:"gg123",
+    lessonsCompleted:[0, 2, 1, 5]
+});
+
+console.log(users) */
+
+
+
+///////DOM//////////
+////////////////////
+////////////////////
+
+//Document Object Model allows you  to access and change the styiling and content of elements on your website
+
+
+
+
+/* // First way of accessing an element
+console.log(document.querySelector('h1'));
+
+
+// second way
+console.log(document.getElementById('title'))
+
+// class - .       id - # */
+
+
+//change HTML
+/* document.querySelector('.title').innerHTML += 'Frontend Simplified'
 
 
 
 
 
+//Change CSS
 
+document.querySelector(".title").style.dontSize = '16px'
 
+function toggleDarkMode() {
+    document.querySelector(".title").style.color = 'red'
 
+} */
 
-
+function toggleDarkMode() {
+document.querySelector('body').classList.toggle("dark-theme")
+}
